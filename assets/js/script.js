7f0a002937cc744ffc201db30f09c23ea88e40f1
Monika <raciam@oregonstate.edu>
2023-06-05 06:10:33 -0500

    Add .ready() function; add and delete comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+// Wait for contents of document to load before running Javascript
+// Long form version of $(function() {})
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -18,18 +17,20 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
+  // Display current date in header of page
   function displayDate() {
     var currDate = dayjs();
-    $('#currentDay').text(currDate.format('dddd, MMMM D'));
+    $('#currentDay').text(currDate.format('dddd, MMMM'));
   }
 
   displayDate();
-  setInterval(displayDate, 1000);
 
+  // Update displayed date every minute
+  setInterval(displayDate, 60000);
 
 });
+
+
 
 
